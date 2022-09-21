c09d448d0f1a4a1fe4f7286080e1222ad784850a
Razieh Abedi <raziyeabedi@gmail.com>
2022-09-21 12:17:24 +0800

    wrote the aside toggle

--- Assets/js/App.js
+++ Assets/js/App.js
@@ -1,32 +1,37 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  "use strict";
 
-  // document.querySelector('.menu-btn').addEventListener("click",()=>{
-  
-  //   if (document.querySelector("aside").style.marginLeft=="0px") {
-  //     document.querySelector("aside").style.marginLeft="-300px"
-  //   } else {
-  //     document.querySelector("aside").style.marginLeft="0px"
-  //   }
-      
-  // })
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
 
-  // document.
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+// document.querySelector('.menu-btn').addEventListener("click",()=>{
+
+//   if (document.querySelector("aside").style.marginLeft=="0px") {
+//     document.querySelector("aside").style.marginLeft="-300px"
+//   } else {
+//     document.querySelector("aside").style.marginLeft="0px"
+//   }
+
+// })
+
+document.querySelector(".nav-list").addEventListener("click", () => {
+  document.querySelector("aside").style.display = "none";
+});
